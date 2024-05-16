36fdeaeaa760cf81f64dae4a59f386231265f2f9
Ubieeel <ultimacontagabi@outlook.com>
2024-05-15 22:36:56 -0300

    Primeiro Commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function AbrirLogin() {
+    document.getElementById("login").style.display = "block";
+  }
+  
+  function FecharLogin() {
+    document.getElementById("login").style.display = "none";
+  }
