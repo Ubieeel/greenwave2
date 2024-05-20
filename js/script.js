4686f19da3bb25562cc17c5e645b7615fbfd0955
Ubieeel <ultimacontagabi@outlook.com>
2024-05-20 01:14:58 -0300

    Add Carrossel

--- js/script.js
+++ js/script.js
@@ -5,3 +5,23 @@ function AbrirLogin() {
   function FecharLogin() {
     document.getElementById("login").style.display = "none";
   }
+
+  // parte carrossel
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaimg()
+}, 5000)
+
+function proximaimg(){
+    cont++
+
+    if(cont >5){
+      cont = 1  
+    }
+
+    document.getElementById('radio'+cont).checked = true
+}
